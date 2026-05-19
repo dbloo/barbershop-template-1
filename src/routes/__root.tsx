83244ed8130e49940ae4a87b4f54722ba29d5a12
Dominic Bloomfield <nooboon14@gmail.com>
2026-05-19 16:02:33 -0400

    style

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -17,7 +17,7 @@ export const Route = createRootRoute({
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'The Luxe Barber Suite - Hialeah’s Premier Barbershop',
+        title: 'The Luxe Barber Suite - Fort Lauderdale\'s Premier Barbershop',
       },
     ],
     links: [
