b4c7d1038980fe7c2497e5b4893fb1ff985cccb1
Dominic Bloomfield <nooboon14@gmail.com>
2026-06-03 16:17:20 -0400

    new template

--- src/siteinfo.tsx
+++ src/siteinfo.tsx
@@ -1,10 +1,10 @@
 import { Scissors, Lollipop} from 'lucide-react'
 
-export const title = "Nelly Da Barber"
-export const location = "Margate, FL"
+export const title = "Refined Cuts"
+export const location = "Hollywood, FL"
 export const keywords = `${title}, barbershop, ${location}, grooming services, haircuts, fades, beard trims`
 export const herodescription = `Your premier barbershop in ${location}. Book your appointment today and experience the best`
-export const about = `With ${title}, we are dedicated to providing top-notch grooming services in ${location}. Our skilled barbers specialize in a wide range of haircuts, fades, and beard trims, ensuring that every client leaves looking and feeling their best. With a commitment to quality and customer satisfaction, we strive to create a welcoming and comfortable environment for all our clients. Book your appointment today and experience the best grooming services in town at The Luxe Barber Suite!`
+export const about = `At ${title}, we are dedicated to providing top-notch grooming services in ${location}. Our skilled barbers specialize in a wide range of haircuts, fades, and beard trims, ensuring that every client leaves looking and feeling their best. With a commitment to quality and customer satisfaction, we strive to create a welcoming and comfortable environment for all our clients. Book your appointment today and experience the best grooming services in town at The Luxe Barber Suite!`
 export const intro = `Experience top-notch grooming services with ${title}, your premier barbershop in ${location}. Book your appointment today for a fresh fade, stylish haircut, and exceptional grooming services. Our skilled barbers are dedicated to providing you with the best grooming experience in town. Don't wait, schedule your visit now and step out in style at ${title}`
 export const link = " "
 export const colors = {
@@ -28,8 +28,8 @@ export const hours = {
 }
 
 export const contactInfo = {
-    address: "1442 N State Rd 7, Margate, FL 33063",
-    phone: "(954) 588-1503",
+    address: "5911 Johnson St, Hollywood, FL 33021",
+    phone: "(954) 872-7922",
 }
 
 export const images = {
